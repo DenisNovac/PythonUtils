415dc72d8e053c719dfd649481818409f800d588
DenisNovac <ponydnk@gmail.com>
2019-03-19 01:55:40 +0500

    Right group descriptor reading and correct comments in findInodeTableOffset(...)

--- EXT4RawInodeSearcher/ext4_raw_inode_searcher.py
+++ EXT4RawInodeSearcher/ext4_raw_inode_searcher.py
@@ -1,11 +1,3 @@
-# This program is raw inode searcher for any files in EXT4 systems.
-# You need to give it two arguments: path to your file and name of your device
-# in /dev/ folder (such as /dev/sda1). Then program will search its raw inode
-# data in file system ext4.
-
-# This program must be executed by SUPERUSER (due to access to /dev/) and only in Python 2.7
-# Documentation: https://ext4.wiki.kernel.org/index.php/Ext4_Disk_Layout
-
 '''
 Copyright (c) 2019 UrFU, Denis Yablochkin (yd.novac@gmail.com), Egor Maximov
 
@@ -27,6 +19,15 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
+
+# This program is raw inode searcher for any files in EXT4 systems.
+# You need to give it two arguments: path to your file and name of your device
+# in /dev/ folder (such as /dev/sda1). Then program will search its raw inode
+# data in file system ext4.
+
+# This program must be executed by SUPERUSER (due to access to /dev/) and only in Python 2.7
+# Documentation: https://ext4.wiki.kernel.org/index.php/Ext4_Disk_Layout
+
 
 import os,sys,struct,math
 
@@ -67,7 +68,12 @@ def readSuperblockInfo ( pathToDevice ):
     # Offset of a superblock
     sb_offset=0x400
     file.seek(sb_offset)
+
+    # whole superblock for ext4_inode_checksum_calculator module
+    whole_superblock=file.read(1024)
+
     # Array with all we need
+    file.seek(sb_offset)
     bytes=file.read(0x68+0x10)
     #print(bytes.encode('hex'))
     file.close()
@@ -86,8 +92,8 @@ def readSuperblockInfo ( pathToDevice ):
     # is correct by executing ls -l /dev/disk/by-uuid/
     # and also we do not need to reverse it since it is not little endian
     s_uuid=bytes[0x68:0x68+16]
-    #print(list(s_uuid))
-    sb_info=[s_blocks_per_group, s_inodes_per_group, s_inode_size, block_size]
+
+    sb_info=[s_blocks_per_group, s_inodes_per_group, s_inode_size, block_size, whole_superblock]
     return sb_info
 
 
@@ -109,35 +115,38 @@ def findInodeTableOffset ( pathToDevice, inodeNum, sb_info ):
     s_inodes_per_group=sb_info[1]
     block_size=sb_info[3]
 
-    # group number is always lesser integer
+    # group number is always lesser integer of the fraction
     groupNumber=(inodeNum-1)/s_inodes_per_group
     groupNumber=int(math.floor(groupNumber))
 
-    # find place where inode table offset is written
-    inodeTableAddress=groupNumber*64+block_size
+    # find offset to the group descriptor (size is 64 bytes) of the group
+    groupDescriptorOffset=groupNumber*64+block_size
 
-    # now we need to jump to this offset to find the offset with inode table
-    # where our inode is stored
+    # now we need to jump to this offset in group descriptor
+    # to find the offset with inode table where our inode is stored
     file=open(pathToDevice,"r")
-    file.seek(inodeTableAddress)
-    bytes=file.read(0x8+0x28)
+    file.seek(groupDescriptorOffset)
+
+    # inode table offets in group descriptor:
+    # 0x8 	__le32 	bg_inode_table_lo
+    # 0x28 	__le32 	bg_inode_table_hi
+    bytes=file.read(0x28+4) # read all descriptor up to high inode table offset
     file.close()
     #print(bytes.encode('hex'))
 
-    # here we read high and low parts for number of block with inode inode
-    # where our inode stored
+    # here we read high and low parts for offset to inode table where
+    # our inode stored
     bg_inode_table_lo=bytes[0x8:0x8+4]
     bg_inode_table_hi=bytes[0x28:0x28+4]
 
-    # here we make one number from parts in BIG ENDIAN (we used reversed)
+    # make one number from parts in BIG ENDIAN (we used reversed)
     bg_inode_table=list(reversed(bg_inode_table_hi))+list(reversed(bg_inode_table_lo))
     inode_table_block_string="".join(bg_inode_table)
 
     # converting it to integer
     inode_table_block=struct.unpack(">Q",inode_table_block_string)
 
-    # making absolute offset to a block where inode table with needed
-    # inode starts
+    # making absolute offset to inode table with needed inode
     inode_table_offset=inode_table_block[0]*block_size
 
     return inode_table_offset
@@ -168,7 +177,7 @@ def readInode ( pathToDevice, sb_info, inodeNum, inode_table_offset ):
     f = open("output.bin","w+b")
     f.write(bytes)
     f.close()
-    return 1
+    return bytes
 
 
 # error message if too few arguments given
@@ -186,11 +195,13 @@ def main ( args ):
     pathToFile=args[1]
     pathToDevice=args[2]
 
-    inodeNum=readInodeNumber(pathToFile)
-    print("Inode number: "+str(inodeNum))
+    inode_num=readInodeNumber(pathToFile)
+    print("Inode number: "+str(inode_num))
     print("")
 
     sb_info=readSuperblockInfo(pathToDevice)
+
+
     print("Superblock info:")
     print("Blocks per group: "+str(sb_info[0]))
     print("Inodes per group: "+str(sb_info[1]))
@@ -198,17 +209,23 @@ def main ( args ):
     print("Block size: "+str(sb_info[3]))
     print("")
 
-    inode_table_offset=findInodeTableOffset(pathToDevice,inodeNum,sb_info)
+    inode_table_offset=findInodeTableOffset(pathToDevice,inode_num,sb_info)
     print("Inode table offset: "+str(inode_table_offset))
 
-    readInode(pathToDevice,sb_info,inodeNum,inode_table_offset)
+    raw_inode=readInode(pathToDevice,sb_info,inode_num,inode_table_offset)
     print("")
     print("SEE THE OUTPUT.BIN FILE FOR ANALYSIS OF YOUR INODE")
 
+    # return for ext4_inode_checksum_calculator module
+    raw_superblock = sb_info[4] # for ext4_inode_checksum_calculator
+    s_inode_size=sb_info[2]
+    return [inode_num, s_inode_size, raw_inode, raw_superblock]
 
 
-
-# Start :)
-print("-=Inode Searcher for EXT4=-")
-if (len(sys.argv)<3): errorMsg()
-main(sys.argv)
+# need this if to not execute code while importing it inside
+# ext4_inode_checksum_calculator
+if __name__ == "__main__":
+    # Start :)
+    print("-=Inode Searcher for EXT4=-")
+    if (len(sys.argv)<3): errorMsg()
+    main(sys.argv)
